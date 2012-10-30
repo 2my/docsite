e4630d0a1a242cb7f3890bb1b49ce8ae2edb59cd
Tommy Skodje <tsk@antares.no>
2012-10-31 00:47:29 +0100

    Controller, view and route for showing MarkDown

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,28 +14,4 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def page( resourceName: String ) = Action {
-    val resource	=
-      Play.resource( "/public/" + resourceName )
-      .orElse( Play.resource( "/public/" + resourceName.replaceAll( "html\\z", "md") ) )
-      .map( url => new File(url.getFile) )
-    resource match {
-      case None => NotFound
-      case Some(file) if isMarkDownResult( file ) => markDownResult( file )
-      case Some(file) => Ok.sendFile( file, true )
-    }
-  }
-
-  private def isMarkDownResult( file: File ) = file.getName().endsWith( ".md" );
-
-  /** Needed because markdown files not mime mapped */
-  private def markDownResult( mdFile: File ) = SimpleResult(
-	        header = ResponseHeader(OK, Map(
-	          CONTENT_LENGTH -> mdFile.length.toString,
-	          CONTENT_TYPE -> "text/plain"
-	        )),
-	        Enumerator.fromFile( mdFile )
-	      )
-	    ;
-
 }
